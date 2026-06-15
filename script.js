7e3fbb0be9518b3b9db8cda65dd1559d5b5b7893
nabaafatima <nabaafatima7097@gmail.com>
2026-06-15 20:15:53 +0500

    made the logic for reset button

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const hello = document.getElementById("button");
 const area = document.getElementById("text");
+
 hello.addEventListener("click",()=>{
-    area.textContent = "Hello";
+    if(area.textContent === "") {
+      area.textContent = "Hello";  
+    } else {
+        let c = area.textContent;
+        area.textContent = c + " Hello";
+    }
 });
+const remove = document.getElementById("reset");
+remove.addEventListener("click",()=>{
+    area.textContent = "";
+})
