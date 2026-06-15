e0e9f1cbe4e431c2c75f81ff80e65280e31b0d8c
nabaafatima <nabaafatima7097@gmail.com>
2026-06-15 20:17:01 +0500

    made it look neat and done!

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const hello = document.getElementById("button");
 const area = document.getElementById("text");
-
 hello.addEventListener("click",()=>{
     if(area.textContent === "") {
       area.textContent = "Hello";  
